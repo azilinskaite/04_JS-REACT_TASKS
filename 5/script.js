54246d356cca72307b7ca18d87246a2cb42fdfb7
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-01-08 16:30:16 +0100

    Task 5: API & Promises

--- 5/script.js
+++ 5/script.js
@@ -1,26 +1,38 @@
 // Base URL
-const apiUrl = "https://api.example.com/data";
+/const apiUrl = "https://opentdb.com/api.php?amount=1";
 
-// Function to make a GET request using
-// TODO: 1. find the error and fix it, 2. finish the function
+// Function to make a GET request
 function fetchData(url) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem:", error);
     });
 }
 
 fetchData(apiUrl);
 
 // Function to make a POST request
-// TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error))
+}
 
 const dataToPost = {
   name: "John Doe",
