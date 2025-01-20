c2995357030cd080befc92fac034dd3fa7cfe03c
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-01-20 20:11:45 +0100

    Task 9: REACT form

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,24 +1,16 @@
 import "./App.css";
-import PropsComponent from "./Components/MyStateComponent/PropsComponent";
+import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
 
 function App() {
-  const person = {
-    name: "John",
-    info: {
-      age: 25,
-      city: "New York",
-      job: "Developer",
-      hobbies: ["reading", "coding", "gaming"],
-    },
-  };
 
   return (
-    <div>
+    <div className="container">
       <header>
-        <h1>This is React</h1>
+        <h1>MYSTERY NEWSLETTER</h1>
       </header>
-      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <PropsComponent person={person} />
+      <main>
+      <MyStateComponent/>
+      </main>
     </div>
   );
 }
--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 
-const MyStateComponent = () => {
   // TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
+
+const MyStateComponent = () => {
+  const [name, updateName] = useState("");
+  const [age, updateAge] = useState("");
+
+  // event listener function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Name: ", name);
+    console.log("Age: ", age);
+  }; 
+
   return (
     <div>
-      <h1>My State Component</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="usersName">What's your name?</label><br />
+        <input type="text" name="usersName" value={name} onChange={(e) => updateName(e.target.value)}/><br />
+        <label htmlFor="usersAge">How old are you?</label><br />
+        <input type="number" min="1" max ="150" name="usersAge" value={age} onChange={(e) => updateAge(e.target.value)}/><br/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
