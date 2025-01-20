ad812ee5eeab3ef13efefb3894ace69d41bf7201
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-01-20 23:06:38 +0100

    Task 10: display data from API

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,24 +1,14 @@
 import "./App.css";
-import PropsComponent from "./Components/MyStateComponent/PropsComponent";
+import { EffectsComponent } from "./Components/Effect/EffectsComponent.jsx";
 
 function App() {
-  const person = {
-    name: "John",
-    info: {
-      age: 25,
-      city: "New York",
-      job: "Developer",
-      hobbies: ["reading", "coding", "gaming"],
-    },
-  };
 
   return (
     <div>
       <header>
-        <h1>This is React</h1>
+        <h1>Game of Thrones characters</h1>
       </header>
-      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <PropsComponent person={person} />
+      <EffectsComponent/>
     </div>
   );
 }
